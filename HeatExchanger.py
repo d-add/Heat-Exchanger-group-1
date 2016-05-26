10bc090312eec34e155f607404a8bcca97134ef0
d-add <danieladdington2005@gmail.com>
2016-05-26 17:49:51 -0600

    Update HeatExchanger.py
    Added Class "Fluid" with and imported Thermophysical_Properties.csv

--- HeatExchanger.py
+++ HeatExchanger.py
@@ -43,35 +43,117 @@ class HeatExchanger():
     
     #creat a class for table values
     
-    class Fluid:
-        compound_name = ''
-        Mol_Wt = []
-        MP = []
-        BP = []
-        Cp = []
+    import numpy as np
+import pandas as pd
+
+#Importing the File
+
+thermo_file = pd.read_csv('Thermophysical_Properties.csv')
+
+
+
+#Assigning Variables to the Columns
+
+compound_name = thermo_file['Compound Name']
+mol_wt = thermo_file['Molecular Weight']
+melt_pt = thermo_file['Melting Point (K)']
+boil_pt = thermo_file['Normal Boiling Point (K)']
+cp_A = thermo_file['A']
+cp_B = thermo_file['B']
+cp_C = thermo_file['C']
+cp_D = thermo_file['D']
+cp_E = thermo_file['E']
+
+
+#Creating the Class
+
+class Fluid:
+    name = []
+    def c_name(self, name):
+        self.name.append(name)
         
-        def findMP(self, MP):
-            self.MP.append(MP)
+    Mol_Wt = []
+    def c_mw(self, Mol_Wt):
+        self.name.append(Mol_Wt)
+    
+    MP = []
+    def c_mp(self, MP):
+        self.name.append(MP)
         
-    com1 = Fluid()
-    com2 = Fluid()
-    com3 = Fluid()
-    com4 = Fluid
-    
-    com1.compound_name = 'Water'
-    com2.compound_name = 'R134a'
-    com3.compound_name = 'Ethanol'
-    com4.compound_name = '2,2,4-trimethlypentane'
-    '''
-    com1.findMP(273.15)
-    com2.findMP(172.00)
-    com3.findMP(159.05)
-    com4.findMP(165.777)
-    '''
-    
-    
-    
+    BP = []
+    def c_bp(self, BP):
+        self.name.append(BP)
         
+    A = []
+    def c_a(self, A):
+        self.name.append(A)
+        
+    B = []
+    def c_b(self, B):
+        self.name.append(B)
+        
+    C = []
+    def c_c(self, C):
+        self.name.append(C)
+        
+    D = []
+    def c_d(self, D):
+        self.name.append(D)
+        
+    E = []
+    def c_e(self, E):
+        self.name.append(E)
+        
+    
+C = np.empty(4)
+C0 = Fluid()
+C1 = Fluid()
+C2 = Fluid()
+C3 = Fluid()
+
+
+C0.name = compound_name[0]
+C0.Mol_Wt = mol_wt[0]
+C0.MP = melt_pt[0]
+C0.BP = boil_pt[0]
+C0.A = cp_A[0]
+C0.B = cp_B[0]
+C0.C = cp_C[0]
+C0.D = cp_D[0]
+C0.E = cp_E[0]
+    
+C1.name = compound_name[1]
+C1.Mol_Wt = mol_wt[1]
+C1.MP = melt_pt[1]
+C1.BP = boil_pt[1]
+C1.A = cp_A[1]
+C1.B = cp_B[1]
+C1.C = cp_C[1]
+C1.D = cp_D[1]
+C1.E = cp_E[1]
+    
+C2.name = compound_name[2]
+C2.Mol_Wt = mol_wt[2]
+C2.MP = melt_pt[2]
+C2.BP = boil_pt[2]
+C2.A = cp_A[2]
+C2.B = cp_B[2]
+C2.C = cp_C[2]
+C2.D = cp_D[2]
+C2.E = cp_E[2]
+    
+C3.name = compound_name[3]
+C3.Mol_Wt = mol_wt[3]
+C3.MP = melt_pt[3]
+C3.BP = boil_pt[3]
+C3.A = cp_A[3]
+C3.B = cp_B[3]
+C3.C = cp_C[3]
+C3.D = cp_D[3]
+C3.E = cp_E[3]
+    
+    
+    
     
     
     
